6ae39c700c75a92cd569a0fd03c274d2af0abe95
Kyle Schutt <kyle@revv.co>
2017-01-20 14:39:17 -0500

    remove self << class

--- lib/cloudflare/client/services/zone_service.rb
+++ lib/cloudflare/client/services/zone_service.rb
@@ -8,90 +8,88 @@ module Cloudflare
   module Client
     module Services
       class ZoneService < BaseService
-        class << self
-          def zone_list(params = nil)
-            url = Util::Config.get('client.base_url') + Util::Config.get('client.zones') 
-            url = build_url(url, params)
+        def zone_list(params = nil)
+          url = Util::Config.get('client.base_url') + Util::Config.get('client.zones') 
+          url = build_url(url, params)
 
-            get_act(url, params)
-          end
+          get_act(url, params)
+        end
 
-          def zone_ssl_verification(zone_id)
-            raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
+        def zone_ssl_verification(zone_id)
+          raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
 
-            url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_ssl_verification'), zone_id)
+          url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_ssl_verification'), zone_id)
 
-            get_act(url)
-          end
+          get_act(url)
+        end
 
-          def zone_ssl_settings(zone_id, value)
-            raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
-            raise Exceptions::ServiceException, "Value is required (off, flexible, full, full_strict)." if value.nil? || !(%w(off flexible full full_strict).include?(value.downcase))
-            params = { 
-              value: value.downcase
-            }
+        def zone_ssl_settings(zone_id, value)
+          raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
+          raise Exceptions::ServiceException, "Value is required (off, flexible, full, full_strict)." if value.nil? || !(%w(off flexible full full_strict).include?(value.downcase))
+          params = { 
+            value: value.downcase
+          }
 
-            url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_ssl_settings'), zone_id)
+          url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_ssl_settings'), zone_id)
 
-            patch_act(url, params.to_json)
-          end
+          patch_act(url, params.to_json)
+        end
 
-          def get_dns_record(zone_id, dns_record_id)
-            raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
-            raise Exceptions::ServiceException, "DNS Record ID is required." if dns_record_id.nil?
+        def get_dns_record(zone_id, dns_record_id)
+          raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
+          raise Exceptions::ServiceException, "DNS Record ID is required." if dns_record_id.nil?
 
-            url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_dns_records_crud'), zone_id, dns_record_id)
+          url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_dns_records_crud'), zone_id, dns_record_id)
 
-            get_act(url)
-          end
+          get_act(url)
+        end
 
-          def create_dns_record(zone_id, type, name, content, ttl=120, proxied=true)
-            raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
-            raise Exceptions::ServiceException, "Type is required (A, AAAA, CNAME, TXT, SRV, LOC, MX, NS, SPF)." if type.nil? || !(%w(A AAAA CNAME TXT SRV LOC MX NS SPF).include?(type.upcase))
-            raise Exceptions::ServiceException, "Name is required." if name.nil?
-            raise Exceptions::ServiceException, "Content is required." if content.nil?
+        def create_dns_record(zone_id, type, name, content, ttl=120, proxied=true)
+          raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
+          raise Exceptions::ServiceException, "Type is required (A, AAAA, CNAME, TXT, SRV, LOC, MX, NS, SPF)." if type.nil? || !(%w(A AAAA CNAME TXT SRV LOC MX NS SPF).include?(type.upcase))
+          raise Exceptions::ServiceException, "Name is required." if name.nil?
+          raise Exceptions::ServiceException, "Content is required." if content.nil?
 
-            params = {
-              type: type,
-              name: name,
-              content: content,
-              ttl: ttl,
-              proxied: proxied
-            }
+          params = {
+            type: type,
+            name: name,
+            content: content,
+            ttl: ttl,
+            proxied: proxied
+          }
 
-            url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_dns_records'), zone_id)
+          url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_dns_records'), zone_id)
 
-            post_act(url, params.to_json)
-          end
+          post_act(url, params.to_json)
+        end
 
-          def update_dns_record(zone_id, dns_record_id, type, name, content, ttl=120, proxied=true)
-            raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
-            raise Exceptions::ServiceException, "DNS Record ID is required." if dns_record_id.nil?
-            raise Exceptions::ServiceException, "Type is required (A, AAAA, CNAME, TXT, SRV, LOC, MX, NS, SPF)." if type.nil? || !(%w(A AAAA CNAME TXT SRV LOC MX NS SPF).include?(type.upcase))
-            raise Exceptions::ServiceException, "Name is required." if name.nil?
-            raise Exceptions::ServiceException, "Content is required." if content.nil?
+        def update_dns_record(zone_id, dns_record_id, type, name, content, ttl=120, proxied=true)
+          raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
+          raise Exceptions::ServiceException, "DNS Record ID is required." if dns_record_id.nil?
+          raise Exceptions::ServiceException, "Type is required (A, AAAA, CNAME, TXT, SRV, LOC, MX, NS, SPF)." if type.nil? || !(%w(A AAAA CNAME TXT SRV LOC MX NS SPF).include?(type.upcase))
+          raise Exceptions::ServiceException, "Name is required." if name.nil?
+          raise Exceptions::ServiceException, "Content is required." if content.nil?
 
-            params = {
-              type: type,
-              name: name,
-              content: content,
-              ttl: ttl,
-              proxied: proxied
-            }
+          params = {
+            type: type,
+            name: name,
+            content: content,
+            ttl: ttl,
+            proxied: proxied
+          }
 
-            url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_dns_records_crud'), zone_id, dns_record_id)
+          url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_dns_records_crud'), zone_id, dns_record_id)
 
-            put_act(url, params.to_json)
-          end
+          put_act(url, params.to_json)
+        end
 
-          def destroy_dns_record(zone_id, dns_record_id)
-            raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
-            raise Exceptions::ServiceException, "DNS Record ID is required." if dns_record_id.nil?
+        def destroy_dns_record(zone_id, dns_record_id)
+          raise Exceptions::ServiceException, "Zone ID is required." if zone_id.nil?
+          raise Exceptions::ServiceException, "DNS Record ID is required." if dns_record_id.nil?
 
-            url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_dns_records_crud'), zone_id, dns_record_id)
+          url = Util::Config.get('client.base_url') + sprintf(Util::Config.get('client.zones_dns_records_crud'), zone_id, dns_record_id)
 
-            delete_act(url)
-          end
+          delete_act(url)
         end
       end
     end
--- lib/cloudflare/host/services/zone_service.rb
+++ lib/cloudflare/host/services/zone_service.rb
@@ -8,28 +8,26 @@ module Cloudflare
   module Host
     module Services
       class ZoneService < BaseService
-        class << self
-          def zone_lookup(zone_name)
-            raise Exceptions::ServiceException, "Zone Name is required." if zone_name.nil?
-            params = {
-              zone_name: zone_name
-            }
-            post_act(:zone_lookup, params)
-          end
+        def zone_lookup(zone_name)
+          raise Exceptions::ServiceException, "Zone Name is required." if zone_name.nil?
+          params = {
+            zone_name: zone_name
+          }
+          post_act(:zone_lookup, params)
+        end
 
-          def zone_set(zone_name, resolve_to, subdomains)
-            raise Exceptions::ServiceException, "Zone Name is required." if zone_name.nil?
-            raise Exceptions::ServiceException, "Resolve Host is required." if resolve_to.nil?
-            raise Exceptions::ServiceException, "Subdomain is required." if subdomains.nil?
+        def zone_set(zone_name, resolve_to, subdomains)
+          raise Exceptions::ServiceException, "Zone Name is required." if zone_name.nil?
+          raise Exceptions::ServiceException, "Resolve Host is required." if resolve_to.nil?
+          raise Exceptions::ServiceException, "Subdomain is required." if subdomains.nil?
 
-            params = {
-              zone_name: zone_name,
-              resolve_to: resolve_to,
-              subdomains: subdomains
-            }
-            
-            post_act(:zone_set, params)
-          end
+          params = {
+            zone_name: zone_name,
+            resolve_to: resolve_to,
+            subdomains: subdomains
+          }
+          
+          post_act(:zone_set, params)
         end
       end
     end
